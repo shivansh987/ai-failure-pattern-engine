45536b024b6bc0e6bcca3b8f9b15834f0e4af1d4
shivanshc987 <shivanshc0987@outlook.com>
2026-02-08 22:04:48 +0530

    Day 1: Initial ErrorEvent API with POST /api/v1/events

---
+++ src/main/java/com/example/ai_failure_engine/controller/EventController.java
@@ -0,0 +1,25 @@
+package com.example.ai_failure_engine.controller;
+
+import com.example.ai_failure_engine.model.ErrorEvent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/events")
+public class EventController {
+
+    @PostMapping
+    public ResponseEntity<String> receiveEvent(@RequestBody ErrorEvent errorEvent) {
+
+        System.out.println("===== ERROR EVENT RECEIVED =====");
+        System.out.println("Service Name: " + errorEvent.getServiceName());
+        System.out.println("Error Code: " + errorEvent.getErrorCode());
+        System.out.println("Message: " + errorEvent.getMessage());
+        System.out.println("Severity: " + errorEvent.getSeverity());
+        System.out.println("Timestamp: " + errorEvent.getTimestamp());
+        System.out.println("================================");
+
+        return new ResponseEntity<>("Event received successfully", HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/example/ai_failure_engine/model/ErrorEvent.java
@@ -0,0 +1,54 @@
+package com.example.ai_failure_engine.model;
+
+import java.time.LocalDateTime;
+
+public class ErrorEvent {
+    private String serviceName;
+    private String errorCode;
+    private String message;
+    private String severity;
+    private LocalDateTime timestamp;
+
+    public ErrorEvent() {
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
