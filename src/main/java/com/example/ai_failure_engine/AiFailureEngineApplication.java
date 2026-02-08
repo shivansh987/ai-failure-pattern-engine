0445dad6024fb95d89c28b69a9dd0012743b1305
shivanshc987 <shivanshc0987@outlook.com>
2026-02-08 14:33:19 +0530

    Initial Spring Boot setup with Java 19

---
+++ src/main/java/com/example/ai_failure_engine/AiFailureEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_failure_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFailureEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFailureEngineApplication.class, args);
+	}
+
+}
